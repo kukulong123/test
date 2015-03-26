b26a90306d2dee4a14bcce482914920587039f32
wanglong6 <wanglong6@xdf.cn>
2015-03-26 11:03:43 +0800

    aa

--- src/te.java
+++ src/te.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class te {
 	public static void main(String[] args) {
-		System.out.println("aa");
+		long longValue = 3000000l;
+		System.out.println(longValue);
+		
 	}
 }
